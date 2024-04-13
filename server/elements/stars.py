d5923899b5195dbb1a138960461c2d2b29401d0f
Nazar Bibik <nazar.bibik456@gmail.com>
2024-04-14 00:34:37 +0200

    Renaming a module to a more intuitive name

---
+++ server/elements/stars.py
@@ -0,0 +1,31 @@
+from server import Element
+
+
+# class Stars(Element):
+#     def path(self) -> str:
+#         return "stars"
+
+#     def children(self) -> list['Element']:
+#         return
+
+#     def html(self) -> str:
+#         return "stars.html"
+
+#     def options(self) -> bool:
+#         return True
+    
+
+def init(get: str = None):
+    name = "stars"
+    path = "stars"
+    is_embeded = False
+    html = "stars.html"
+    css = {}
+    js = {}
+    children = {}
+
+    if get == "name":
+        return name
+
+    return Element(name, path, is_embeded, html,\
+                    None if css == {} else css, None if js == {} else js, None if children == {} else children)
